79eac85ae59197bbce8bf498e6796175c7a7afbd
Kaylee Schwab <90537705+KayleeSchwab@users.noreply.github.com>
2021-09-12 14:05:26 -0500

    ignore .env

--- bot.py
+++ bot.py
@@ -14,4 +14,12 @@ async def on_ready():
     for guild in client.guilds:
         print(guild)
 
+# when someone posts in a quote channel log the quote, parse it, increment score for user tagged
+
+# when someone types "quote unclaimed" bot should post a list of top 50 names that are not claimed
+
+# when someone types "quote iclaim [tag]" bot should link user to this tag
+
+# when someone types "quote scoreboard" bot should list the top 5 tags and their scores
+
 client.run(TOKEN)
